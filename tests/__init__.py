e06a31fd572f1c80d44e1eb17d7e55b630a8175e
Arefeh Yavary <yavary.rf@gmail.com>
2024-08-10 09:26:45 -0700

    Update __init__.py

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,1 +1,35 @@
 # Test suite for the project
+# This file allows the tests directory to be treated as a Python package.
+
+# Common fixtures or utilities for tests
+
+def create_test_environment():
+    """Setup a test environment before test cases are run."""
+    # Setup code here
+
+def teardown_test_environment():
+    """Clean up after tests are run."""
+    # Teardown code here
+
+def common_setup_function():
+    """A common setup function that can be used across multiple test modules."""
+    return "setup done"
+
+def common_teardown_function():
+    """A common teardown function for cleaning up after tests."""
+    return "cleanup done"
+
+
+from . import create_test_environment, teardown_test_environment
+
+class TestSomething(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        create_test_environment()
+
+    @classmethod
+    def tearDownClass(cls):
+        teardown_test_environment()
+
+    def test_something(self):
+        pass
