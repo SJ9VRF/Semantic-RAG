ff9d081ca84a72e7498407eebe1761a7de70ec4b
Arefeh Yavary <yavary.rf@gmail.com>
2024-08-10 09:14:36 -0700

    Update pdf_parser.py

--- src/pdf_parser.py
+++ src/pdf_parser.py
@@ -1,1 +1,18 @@
 # Class for parsing PDF files
+from unstructured.partition.pdf import partition_pdf
+
+class PDFParser:
+    def __init__(self, config):
+        self.config = config
+
+    def parse(self):
+        return partition_pdf(
+            filename=self.config.PDF_PATH,
+            extract_images_in_pdf=True,
+            infer_table_structure=True,
+            chunking_strategy="by_title",
+            max_characters=4000,
+            new_after_n_chars=3800,
+            combine_text_under_n_chars=2000,
+            image_output_dir_path="."
+        )
