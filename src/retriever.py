bf99451128ff02aceca5b4d9276781061dcb47bf
Arefeh Yavary <yavary.rf@gmail.com>
2024-08-10 09:19:02 -0700

    Update retriever.py

--- src/retriever.py
+++ src/retriever.py
@@ -1,1 +1,16 @@
 # Class for retrieving document segments
+from langchain.retrievers.multi_vector import MultiVectorRetriever
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.storage import InMemoryStore
+from langchain.vectorstores import Chroma
+
+class ContentRetriever:
+    def __init__(self, config):
+        self.retriever = MultiVectorRetriever(
+            vectorstore=Chroma(collection_name="summaries", embedding_function=OpenAIEmbeddings()),
+            docstore=InMemoryStore(),
+            id_key="doc_id"
+        )
+
+    def retrieve(self, query):
+        return self.retriever.retrieve(query)
