91b034195d2a7b7f9efea50d37cfd0b389152aff
Arefeh Yavary <yavary.rf@gmail.com>
2024-08-10 09:19:39 -0700

    Update element_classifier.py

--- src/element_classifier.py
+++ src/element_classifier.py
@@ -1,1 +1,11 @@
 # Class for classifying PDF elements
+class ElementClassifier:
+    def classify(self, elements):
+        category_counts = {}
+        for element in elements:
+            category = str(type(element))
+            if category in category_counts:
+                category_counts[category] += 1
+            else:
+                category_counts[category] = 1
+        return category_counts
